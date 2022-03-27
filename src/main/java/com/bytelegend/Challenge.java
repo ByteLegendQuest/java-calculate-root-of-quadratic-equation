6ea13f0c1cdccc15adf84c8dd9eb89e41682a9f6
gtn1024 <tony.bazhang@gmail.com>
2022-03-27 08:59:24 +0000

    Update at 2022-03-27T08:59:24.668054Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -20,6 +20,19 @@ public class Challenge {
      * <p>If there is no root, return an empty array.
      */
     public static double[] calculate(int a, int b, int c) {
-        return null;
+        double delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            // 无根
+            return new double[0];
+        } else if (delta == 0) {
+            // 唯一根
+            double root = (-b) / (2 * a);
+            return new double[]{root};
+        } else {
+            // 两个根
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[]{root1, root2};
+        }
     }
 }
