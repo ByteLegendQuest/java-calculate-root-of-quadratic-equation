cccd6d49173d6b19777c1071b1f02408ec888cef
hupizhu <2815695640@qq.com>
2022-04-03 05:17:46 +0000

    Update at 2022-04-03T05:17:46.054366Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -20,8 +20,8 @@ public class Challenge {
      * <p>If there is no root, return an empty array.
      */
     public static double[] calculate(int a, int b, int c) {
-        double root1 = (-b + Math.sqrt(b*b - 4 * a * c) / 2 * a);
-        double root2 = (-b - Math.sqrt(b*b - 4 * a * c) / 2 * a);
+        double root1 = -b + Math.sqrt(b * b - 4 * a * c) / 2 * a;
+        double root2 = -b - Math.sqrt(b * b - 4 * a * c) / 2 * a;
         if (root1 == root2) {
             return new double[] {root1};
         } else if (Double.isNaN(root1) || Double.isNaN(root2)) {
