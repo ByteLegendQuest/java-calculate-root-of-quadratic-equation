831086e4f29eaadf9e0c0a04c99be898f7bf065f
yunghf <yung.hf@outlook.com>
2022-04-04 09:00:27 +0000

    Update at 2022-04-04T09:00:27.315860Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -20,6 +20,19 @@ public class Challenge {
      * <p>If there is no root, return an empty array.
      */
     public static double[] calculate(int a, int b, int c) {
-        return null;
+        double delt = Math.pow(b, 2) - 4 * a * c;
+        if (delt > 0) {
+            double root1 = (- b + Math.sqrt(delt)) / (2 * a);
+            double root2 = (- b - Math.sqrt(delt)) / (2 * a);
+            double[] root = new double[]{ root1, root2 };
+            return root; 
+        } else if (delt == 0) {
+            double root0 = (- b) / (2 * a);
+            double[] root = new double[]{ root0 };
+            return root;
+        } else {
+            double[] root = null;
+            return root;
+        }
     }
 }
