9fc58095c953aff4ee870368682bbdde59bda391
tiger0961 <29515547@qq.com>
2022-10-13 05:08:46 +0000

    Update at 2022-10-13T05:08:46.662234Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,8 +22,8 @@ public class Challenge {
     public static double[] calculate(int a, int b, int c) {
         double[] roots = new double[2];
         double s = Math.sqrt(b * b - 4 * a * c);
-        double root1 = (-1 * b + s) / 2;
-        double root2 = (-1 * b - s) / 2;
+        double root1 = (-1 * b + s) / 2 * a;
+        double root2 = (-1 * b - s) / 2 * a;
 
         if (Double.isNaN(root1) || Double.isNaN(root2)) {
             roots = new double[]{};
