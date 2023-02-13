4258afb45c298f5ef2c3f9b9e984e85028eae0eb
AaronFledermaus <78683536+aaronfledermaus@users.noreply.github.com>
2023-02-13 16:48:20 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -20,6 +20,21 @@ public class Challenge {
      * <p>If there is no root, return an empty array.
      */
     public static double[] calculate(int a, int b, int c) {
-        return null;
+        if(b*b-4*a*c>0){
+            double[] s = new double[2];
+            s[0] = (-b+Math.sqrt(b*b-4*a*c))/2/a;
+            s[1] = (-b-Math.sqrt(b*b-4*a*c))/2/a;
+            return s;
+        }
+        else if(b*b-4*a*c==0){
+            double[] s = new double[1];
+            s[0] = (-b)/2/a;
+            return s;
+        }
+        else{
+            double[] s = new double[0];
+            return s;
+        }
+    }
     }
 }
