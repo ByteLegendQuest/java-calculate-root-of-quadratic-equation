22e7ed8e5543be853ba15b52009e03dff1229d7c
andyliukun <744059787@qq.com>
2023-06-05 09:08:35 +0000

    Update at 2023-06-05T09:08:35.262424Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -20,6 +20,18 @@ public class Challenge {
      * <p>If there is no root, return an empty array.
      */
     public static double[] calculate(int a, int b, int c) {
-        return null;
+        double sq = b * b - 4 * a * c;
+        if (sq < 0) {
+            return new double[0];
+        }
+
+        double sqrt = Math.sqrt(sq);
+        double r1 = (-b + sqrt) / 2 * a;
+        double r2 = (-b - sqrt) / 2 * a;
+        if (r1 != r2) {
+            return new double[]{r1, r2};
+        } else {
+            return new double[]{r1};
+        }
     }
 }
