250dd7a48965ed6e3f255f312764f832e1d202a2
Windsority <mcfengxin@gmail.com>
2023-06-08 04:47:43 +0000

    Update at 2023-06-08T04:47:43.626725Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,7 +22,7 @@ public class Challenge {
     public static double[] calculate(int a, int b, int c) {
         double delta = Math.sqrt(b * b - 4 * a * c);
         if (delta < 0.000000001) {
-            return new double[] { (double)-b / 2 * a };
+            return new double[] { (double) -b / 2 * a };
         }
         return new double[] { (-b + delta) / 2 * a, (-b - delta) / 2 * a};
     }
